e546fa19f427c247f1ac4eaae9cb6f20817f19d8
Diego <iwujigoodluck5@gmail.com>
2025-06-10 21:35:47 +0000

    adding date time

--- script.js
+++ script.js
@@ -4,6 +4,19 @@ const account1 = {
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: "1111",
+
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
 };
 
 const account2 = {
@@ -11,6 +24,19 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: "2222",
+
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
 const account3 = {
@@ -18,6 +44,19 @@ const account3 = {
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: "3333",
+
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
 const account4 = {
@@ -25,6 +64,19 @@ const account4 = {
   movements: [430, 1000, 700, 50, 90, -30],
   interestRate: 1,
   pin: "4444",
+
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-06-25T18:49:59.371Z',
+    // '2020-07-26T12:01:20.894Z',
+    // '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -57,6 +109,7 @@ const closeAccPin = document.querySelector("#closeAccPin");
 const requestLoanBtn = document.querySelector("#requestLoanBtn");
 const inputLoanAmount = document.querySelector("#inputLoanAmount");
 const sortMovements = document.querySelector("#sortMovements");
+const balanceDate = document.querySelector("#balanceDate")
 
 let currentUser;
 // let currentBalance;
@@ -66,7 +119,20 @@ let loggedIn;
 const balanceFun = function (account = currentUser) {
   const balance = account.movements.reduce((acc, cur) => acc + cur);
   return balance.toFixed(2);
+
 };
+
+//BALANCE DATE FUNCTION
+const balDate = function (acc) {
+  const date = new Date(acc);
+  const day = `${date.getDate()}`.padStart(2, 0);
+  const month = `${date.getMonth()}`.padStart(2, 0);
+  const year = date.getFullYear();
+  const hour = date.getHours();
+  const min = date.getMinutes();
+
+  return `${day}/${month}/${year}`;
+}
 
 // SUMMARY DISPLAY FUNCTION
 const summaryDisplay = function (account) {
@@ -91,22 +157,28 @@ const summaryDisplay = function (account) {
   // console.log(IN);
 };
 
-const displayTransaction = function (movements, bool = false) {
+const displayTransaction = function (account, bool = false) {
   transactionDIV.textContent = "";
 
-  const moves = bool ? [...movements].sort((a, b) => a - b) : movements;
+  const moves = bool ? [...account.movements].sort((a, b) => a - b) : account.movements;
 
   let htmlString;
 
   moves.forEach((cur, i) => {
+
+    const movDate = balDate(account.movementsDates[i])
+
     if (cur > 0) {
       htmlString = `<div
                             class="flex items-center justify-between px-8 py-4 border-b border-gray-200">
+                            <div class="flex items-center space-x-2">
                             <div
                             class="bg-gradient-to-tl from-[#39b385] to-[#9be15d] uppercase text-white rounded-full py-0.5 px-1.5">
                             <p class="text-[10px] font-semibold">${
                               i + 1
                             } deposit</p>
+                            </div>
+                  <span id="movementdate">${movDate}</span>
                             </div>
 
                             <div class="ml-auto"><p>${cur.toFixed(2)}€</p></div>
@@ -114,11 +186,14 @@ const displayTransaction = function (movements, bool = false) {
     } else {
       htmlString = `<div
                             class="flex items-center justify-between px-8 py-4 border-b border-gray-200">
+                            <div class="flex items-center space-x-2">
                             <div
                             class="bg-gradient-to-tl from-[#e52a5a] to-[#ff585f] uppercase text-white rounded-full py-0.5 px-1.5">
                             <p class="text-[10px] font-semibold">${
                               i + 1
                             } deposit</p>
+                            </div>
+                  <span id="movementdate">${movDate}</span>
                             </div>
 
                             <div class="ml-auto"><p>${cur.toFixed(2)}€</p></div>
@@ -135,10 +210,11 @@ let bool = true;
 const updateUI = function (account) {
   if (loggedIn) {
     currentUserBalance.textContent = `${balanceFun(account)}€`;
+    balanceDate.textContent = balDate(account.movementsDates[0])
 
     summaryDisplay(currentUser);
 
-    displayTransaction(account.movements);
+    displayTransaction(account);
 
     bool = !bool
   } else {
@@ -250,7 +326,7 @@ requestLoanBtn.addEventListener("click", (e) => {
 // sorting function
 sortMovements.addEventListener("click", () => {
   if (loggedIn) {
-    displayTransaction(currentUser.movements, !bool);
+    displayTransaction(currentUser, !bool);
     bool = !bool;
   }
 });
